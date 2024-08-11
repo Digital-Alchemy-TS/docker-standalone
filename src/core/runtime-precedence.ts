0ebaa63862f88343f3f07899d1cd87b8641791e8
Webber <webber@takken.io>
2024-08-11 19:32:49 +0200

    feat: env precedence

--- src/core/runtime-precedence.ts
+++ src/core/runtime-precedence.ts
@@ -1,38 +1,40 @@
 import { TServiceParams } from '@digital-alchemy/core'
 
-export function RuntimePrecedence({
-  logger,
-  context,
-  config,
-  synapse,
-  hass,
-  lifecycle,
-}: TServiceParams) {
+export function RuntimePrecedence({ logger, config, hass, lifecycle }: TServiceParams) {
   // Whether this runtime is in development mode or not
   const isDevelop = config.homeAutomation.NODE_ENV === 'development'
 
   // When developing locally, the production runtime will pause and the development runtime will take over
-  const isDevelopmentActive = synapse.switch({
-    context,
-    name: 'Whether or not local development takes over from production',
-  })
+  // @ts-expect-error - Entity will be created by setting the state here.
+  const isDevelopmentActive = hass.refBy.id('binary_sensor.is_development_runtime_active')
 
   // Block outgoing commands and most incoming messages in prod when dev overrides it.
   isDevelopmentActive.onUpdate(() => {
-    if (isDevelopmentActive.is_on) {
+    if (isDevelopmentActive.state === 'on') {
       logger.info('Development runtime takes over')
+      // dev takes over, prod pauses
       hass.socket.pauseMessages = !isDevelop
     } else {
       logger.info('Resuming production runtime')
+      // prod resumes, dev pauses
       hass.socket.pauseMessages = isDevelop
     }
   })
 
   // Update the state on startup
-  isDevelopmentActive.is_on = isDevelop
+  lifecycle.onReady(() => {
+    if (isDevelop) isDevelopmentActive.state = 'on'
+  })
 
   // Give the go ahead for production to take over again when shutting down
-  lifecycle.onPreShutdown(() => {
-    if (isDevelop) isDevelopmentActive.is_on = false
+  lifecycle.onPreShutdown(async () => {
+    if (!isDevelop) return
+
+    isDevelopmentActive.state = 'off'
+
+    const result = await isDevelopmentActive.nextState(5000)
+    if (!result) return logger.error(`Unable to verify that production runtime has taken over.`)
+
+    logger.info(`Production runtime has taken over. Development: ${result.state}`)
   })
 }
